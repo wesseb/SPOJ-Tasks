21af31ca9c1258139d79956919b4f1d078aa24e3
wesseb <wesseb95@gmail.com>
2022-05-18 13:19:20 +0200

    start

---
+++ SPOJ-Tasks/Prime_T.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPOJ_Tasks
+{
+    public class Prime_T
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        } 
+    }
+}
